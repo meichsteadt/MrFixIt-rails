c6de37f6c04155d1f872d47a1025216a1c2bb3ff
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-02-24 15:30:49 -0800

    add worker signup test

--- spec/features/worker_pages_spec.rb
+++ spec/features/worker_pages_spec.rb
@@ -7,4 +7,13 @@ describe "workers pages" do
     visit worker_path(worker)
     expect(page).to have_content 'Log Out'
   end
+
+  it "will sign you up as a worker" do
+    visit new_worker_registration_path
+    fill_in 'worker[email]', :with => 'email@worker.com'
+    fill_in 'worker[password]', :with => 'password'
+    fill_in 'worker[password_confirmation]', :with => 'password'
+    click_on 'Sign up'
+    expect(page).to have_content 'Worker Dashboard'
+  end
 end
